dda081002cf143902291d293d4ed283698fec2a0
ValdineyJr <91634407+ValdineyJr@users.noreply.github.com>
2022-01-07 18:30:21 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var dustbinObj, paperObj,groundObject	
+var world;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	paperObj=new paper(200,629,70);
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+
+	Engine.run(engine);
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(230); 
+  
+  paperObj.display();
+  groundObject.display();
+  dustbinObj.display();
+}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+    	Matter.Body.applyForce(paperObj.body,paperObj.body.position,{x:137,y:-145});
+  	}
+}
+
+
+
+
+
